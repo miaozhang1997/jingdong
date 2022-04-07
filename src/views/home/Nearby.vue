282e0ecccb203ca5337eec79e32ffe16b7725796
miaozhang1997 <91865333+miaozhang1997@users.noreply.github.com>
2022-04-07 15:04:01 +0800

    Add files via upload

---
+++ src/views/home/Nearby.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="nearby">
+    <h3 class="nearby__title">附近店铺</h3>
+    <!-- :item="item"给子组件传数据 -->
+    <ShopInfo v-for="item in nearbyList" :key="item._id" :item="item" />
+  </div>
+</template>
+
+<script>
+import { ref } from "vue";
+import { get } from "../../utils/request";
+import ShopInfo from "../../components/ShopInfo";
+
+const useNearbyListEffect = () => {
+  const nearbyList = ref([]);
+  const getNearbyList = async () => {
+    const result = await get("/api/shop/hot-list");
+    if (result?.errno === 0 && result?.data?.length) {
+      nearbyList.value = result.data;
+    }
+  };
+  return { nearbyList, getNearbyList };
+};
+
+export default {
+  name: "Nearby",
+  // 注册局部组件
+  components: { ShopInfo },
+  setup() {
+    const { nearbyList, getNearbyList } = useNearbyListEffect();
+    getNearbyList();
+    return { nearbyList };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../../style/viriables.scss";
+.nearby {
+  &__title {
+    margin: 0.16rem 0 0.02rem 0;
+    font-size: 0.18rem;
+    font-weight: normal;
+    color: $content-fontcolor;
+  }
+}
+</style>
